30596eac002fb09748b20d511b92a491087fa223
Konekonekon <ayanotarumi@gmail.com>
2016-10-05 20:22:53 +0200

    First version. Socket works.

---
+++ src/DrawComponent.java
@@ -0,0 +1,12 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class DrawComponent extends JComponent implements ActionListener {
+
+
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+
+}
