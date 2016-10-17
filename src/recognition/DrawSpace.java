5d507e332b735e52b4c736230138b6a843de4903
Konekonekon <ayanotarumi@gmail.com>
2016-10-17 11:14:06 +0200

    Canvas to draw.

---
+++ src/recognition/DrawSpace.java
@@ -0,0 +1,84 @@
+package recognition;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.util.*;
+
+import javax.swing.*;
+
+public class DrawSpace extends JComponent implements MouseListener, MouseMotionListener {
+
+	private ArrayList<Point> currentLine;
+	private ArrayList<ArrayList<Point>> lines;
+
+
+	public DrawSpace() {
+		super();
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		lines = new ArrayList<ArrayList<Point>>();
+	}
+
+	// TODO : color, thickness for line/text
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		
+		RenderingHints rh = g2.getRenderingHints ();
+	    rh.put (RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+	    rh.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	    g2.setRenderingHints (rh);
+	    
+		g2.setColor(Color.white);
+		g2.fillRect(0, 0, getWidth(), getHeight());
+		g2.setColor(Color.black);
+
+		for (ArrayList<Point> line : lines)
+			this.drawStroke(line, g2);
+	}
+
+	public void drawStroke(ArrayList<Point> line, Graphics2D g2) {
+		int i = 0;
+		while (i < line.size() - 1) {
+			Point p1 = line.get(i);
+			Point p2 = line.get(i + 1);
+			g2.drawLine(p1.x, p1.y, p2.x, p2.y);
+			i++;
+		}
+	}
+
+	// TODO get what user type for "str"
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	public void mousePressed(MouseEvent e) {
+		currentLine = new ArrayList<Point>();
+		lines.add(currentLine);
+		currentLine.add(e.getPoint());
+		repaint();
+	}
+
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		currentLine.add(e.getPoint());
+		repaint();
+	}
+
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	public void mouseMoved(MouseEvent e) {
+
+	}
+
+
+}
