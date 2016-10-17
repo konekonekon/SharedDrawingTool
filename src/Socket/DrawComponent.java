e324b29f1171aff67482d916186f5877496a7ff1
Konekonekon <ayanotarumi@gmail.com>
2016-10-17 11:16:28 +0200

    Socket.

---
+++ src/Socket/DrawComponent.java
@@ -0,0 +1,13 @@
+package Socket;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class DrawComponent extends JComponent implements ActionListener {
+
+
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+
+}
